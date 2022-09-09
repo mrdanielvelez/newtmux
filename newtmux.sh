892bb1674965b8c1883c13c6a157b74f3bc88876
kali <kali@kali.(none)>
2022-09-09 12:43:10 -0400

    Fix escape characters for OSX

--- newtmux.sh
+++ newtmux.sh
@@ -22,27 +22,27 @@ einit() {
 	if alias=`grep -o -h 'alias einit.*' ~/.zshrc ~/.bashrc` || alias=`grep -o -h 'alias engagementinit.*' ~/.zshrc ~/.bashrc` || alias=`grep -o -h 'alias engagement-init.*' ~/.zshrc ~/.bashrc`
 	then
 		einit=`cut -d '=' -f 2 <<< $alias` && einit=`tr -d \'\" <<< $einit` && einit=${einit/#\~/$HOME}
-		read -p $'\e[36mEngagement SKU\e[0m (IPT/EPT/CPT/ADSR/RTA/etc.): ' esku
-		read -p $'\e[31mOpportunity Number: \e[0m' onum
-		read -p $'\e[32mClient Name: \e[0m' cname && cname=`tr ' ' '-' <<< $cname`
-		read -p $'\e[35mYour Initials: \e[0m' yini
+		read -p $'\033[36mEngagement SKU\033[0m (IPT/EPT/CPT/ADSR/RTA/etc.): ' esku
+		read -p $'\033[31mOpportunity Number: \033[0m' onum
+		read -p $'\033[32mClient Name: \033[0m' cname && cname=`tr ' ' '-' <<< $cname`
+		read -p $'\033[35mYour Initials: \033[0m' yini
 		printf '—%.0s' {1..50} && echo
 		$einit -e "$esku" -o "$onum" -c $cname -i "$yini" &>/dev/null
 		if [[ $? -eq 0 ]]
 		then
-			pause && echo -e "\e[33mengagementinit\e[32m completed successfully.\e[0m Continuing..."
+			pause && echo -e "\033[33mengagementinit\033[32m completed successfully.\033[0m Continuing..."
 			project="`pwd`/$cname-$onum-$esku"
 			if [[ -d "$project-02" ]]; then pdir="$project-02"; else pdir="$project-01"; fi
 			LOGOUTPUTDIR="$pdir/Evidence/$cname-notes-$yini/Logging-Output"
 			mkdir "$LOGOUTPUTDIR" &>/dev/null
 			return 0
 		else
-			echo -e "\e[33mengagementinit\e[31m failed to complete\e[0m Continuing..."
+			echo -e "\033[33mengagementinit\033[31m failed to complete\033[0m Continuing..."
 			return 1
 		fi
 	else
-		echo -e "\e[33mengagementinit\e[31m could not be located\e[0m as an alias."
-		echo -e "(\e[36meinit\e[0m, \e[36mengagementinit\e[0m, \e[36mengagement-init\e[0m). Continuing..."
+		echo -e "\033[33mengagementinit\033[31m could not be located\033[0m as an alias."
+		echo -e "(\033[36meinit\033[0m, \033[36mengagementinit\033[0m, \033[36mengagement-init\033[0m). Continuing..."
 		return 1
 	fi
 }
@@ -50,9 +50,9 @@ einit() {
 install_ansi2txt() {
 	if ! [[ `command -v ansi2txt` ]]
 	then
-		echo -e "\e[33mansi2txt\e[0m is\e[31m not installed.\e[0m Would you like to install it?"
-		echo -e "This enables \e[33m$SCRIPT_NAME\e[0m to remove \e[32mANSI color coding\e[0m from log files."
-		echo -n -e "\nCommand:\e[34m sudo apt install colorized-logs \e[0m\c"
+		echo -e "\033[33mansi2txt\033[0m is\033[31m not installed.\033[0m Would you like to install it?"
+		echo -e "This enables \033[33m$SCRIPT_NAME\033[0m to remove \033[32mANSI color coding\033[0m from log files."
+		echo -n -e "\nCommand:\033[34m sudo apt install colorized-logs \033[0m\c"
 		read -n 1 -p "[y | n] " choice && echo
 		if [[ $choice =~ y|Y ]]
 		then
@@ -60,22 +60,22 @@ install_ansi2txt() {
 			if [[ $? -eq 0 ]]
 			then
 				sed -i "s/ansifilter/ansi2txt/" "$HOME/.tmux/plugins/tmux-logging/scripts/start_logging.sh"
-				pause && echo -e "\n\e[33mansi2txt\e[0m was successfully installed and configured. Continuing..."
+				pause && echo -e "\n\033[33mansi2txt\033[0m was successfully installed and configured. Continuing..."
 				return 0
 			else
-				echo -e "Error — \e[33mansi2txt\e[31m failed to install.\e[0m Exiting..."
+				echo -e "Error — \033[33mansi2txt\033[31m failed to install.\033[0m Exiting..."
 				exit 1
 			fi
 		elif [[ $choice =~ n|N ]]
 		then
-			pause && echo -e "Continuing without installing \e[33mansi2txt\e[0m..."
+			pause && echo -e "Continuing without installing \033[33mansi2txt\033[0m..."
 			return 0
 		else
-			echo -e "Error — \e[31mInvalid input.\e[0m Exiting..."
+			echo -e "Error — \033[31mInvalid input.\033[0m Exiting..."
 			exit 1
 		fi
 	else
-		pause && echo -e "\e[33mansi2txt \e[32mis present. \e[0mContinuing..."
+		pause && echo -e "\033[33mansi2txt \033[32mis present. \033[0mContinuing..."
 		sed -i "s/ansifilter/ansi2txt/" "$HOME/.tmux/plugins/tmux-logging/scripts/start_logging.sh"
 	fi
 }
@@ -86,7 +86,7 @@ optimize_config() {
 		if [[ -f "$HOME/.tmux.conf" ]]
 		then
 			date=`date "+%Y-%m-%d"`
-			success_msg="Backed up previous config file to \e[36m$HOME/.tmux.conf.bak-$date\e[0m"
+			success_msg="Backed up previous config file to \033[36m$HOME/.tmux.conf.bak-$date\033[0m"
 			mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak-$date" && pause && echo -e $success_msg
 		fi
 		echo -e 'set-option -g default-shell /bin/zsh\n\n# List of plugins\nset-option -g @plugin "tmux-plugins/tmux-logging"\nset-option -g @plugin "tmux-plugins/tpm"\nset-option -g @plugin "tmux-plugins/tmux-sensible"\n\n# Set command history limit\nset-option -g history-limit 250000\n\n# Disable session renaming\nset-option -g allow-rename off\n\n# Change display-time session option\nset-option -g display-time 750\n\n# Customize Tmux logging output directory\nset-option -g @logging-path ~/tmux-logging-output\nset-option -g @screen-capture-path ~/tmux-logging-output\n\nset-window-option -g mode-keys vi\nbind-key "c" new-window \; run-shell "~/.tmux/plugins/tmux-logging/scripts/toggle_logging.sh"\nbind-key "\"" split-window \; run-shell "~/.tmux/plugins/tmux-logging/scripts/toggle_logging.sh"\nbind-key "%" split-window -h \; run-shell "~/.tmux/plugins/tmux-logging/scripts/toggle_logging.sh"\n\n# Initialize plugins\nrun-shell "~/.tmux/plugins/tpm/tpm"\nrun-shell "~/.tmux/plugins/tmux-logging/logging.tmux"' > "$HOME/.tmux.conf"
@@ -101,14 +101,14 @@ optimize_config() {
 		fi
 		if [[ $? -eq 0 ]]
 		then
-			pause && echo -e "Initialized\e[0m new \e[36m$HOME/.tmux.conf\e[0m with optimal settings..."
+			pause && echo -e "Initialized\033[0m new \033[36m$HOME/.tmux.conf\033[0m with optimal settings..."
 			return 0
 		else
-			echo -e "\e[31mCould not initialize\e[36m $HOME/.tmux.conf.\e[0m Continuing..."
+			echo -e "\033[31mCould not initialize\033[36m $HOME/.tmux.conf.\033[0m Continuing..."
 			return 1
 		fi
 	else
-		pause && echo -e "Using the existing\e[36m ~/.tmux.conf\e[0m file. Continuing..."
+		pause && echo -e "Using the existing\033[36m ~/.tmux.conf\033[0m file. Continuing..."
 		return 0
 	fi
 }
@@ -121,10 +121,10 @@ lower_duration() {
 	sed -i "s/$display_duration\$/`sed 's/5000/750/' <<< $display_duration`/" $shared &>/dev/null
 	if [[ $? -eq 0 ]]
 	then
-		pause && echo -e "Message durations \e[32mwere lowered\e[0m for the \e[33m\"tmux-logging\"\e[0m plugin..."
+		pause && echo -e "Message durations \033[32mwere lowered\033[0m for the \033[33m\"tmux-logging\"\033[0m plugin..."
 		return 0
 	else
-		echo -e "\e[31mMessage durations could not be lowered\e[0m for the \e[36m\"tmux-logging\"\e[0m plugin..."
+		echo -e "\033[31mMessage durations could not be lowered\033[0m for the \033[36m\"tmux-logging\"\033[0m plugin..."
 		return 1
 	fi
 }
@@ -135,7 +135,7 @@ start_tmux() {
 	log="run-shell $HOME/.tmux/plugins/tmux-logging/scripts/toggle_logging.sh"
 	window_0="select-window -t 0" && pane_0="select-pane -t 0"
 	winstring=`[[ $NUM_WINDOWS -ge 2 ]] && echo "windows" || echo "window"` && pause
-	echo -e "Starting \e[33mTmux\e[0m with \e[36m$NUM_WINDOWS\e[0m horizontally-split $winstring..." && $STATUS && sleep 2
+	echo -e "Starting \033[33mTmux\033[0m with \033[36m$NUM_WINDOWS\033[0m horizontally-split $winstring..." && $STATUS && sleep 2
 	case $NUM_WINDOWS in
 		1)
 			tmux new -s $SESSION_NAME \; $log \; split-window \; $log \; $pane_0;;
@@ -152,23 +152,23 @@ start_tmux() {
 	then
 		exit 0
 	else
-		echo -e "Error - \[31mCould not start tmux.\e[0m Exiting..."
+		echo -e "Error - \[31mCould not start tmux.\033[0m Exiting..."
 		exit 1
 	fi
 }
 
 help() {
-	echo -e "\e[36mUsage: \e[32m$SCRIPT_NAME \e[33m-n {tmux-session-name} \e[0m[-w {num_windows} | -e | -k | -s | -h]"
-	echo -e "\e[36m\nMandatory argument:\e[0m"
-	echo -e " \e[33m-n\e[0m | Tmux Session Name"
-	echo -e "\e[36m\nOptional arguments:\e[0m"
-	echo -e " \e[35m-w\e[0m | Specify number of horizontally-split windows to open (default 1, maximum 5)"
-	echo -e " \e[35m-k\e[0m | Use the existing .tmux.conf file instead of an optimized one"
-	echo -e " \e[35m-s\e[0m | Swift and Slient mode (disables status messages)"
-	echo -e " \e[35m-e\e[0m | Run engagementinit.sh before newtmux begins (Alias required)"
-	echo -e " \e[35m-h\e[0m | Display this help menu"
-	echo -e "\n\e[36mExample execution (with an alias in ~/.zshrc or ~/.bashrc):"
-	echo -e " \e[32m$SCRIPT_NAME \e[33m-n\e[0m flast \e[35m-w\e[0m 2 \e[35m-e\e[0m"
+	echo -e "\033[36mUsage: \033[32m$SCRIPT_NAME \033[33m-n {tmux-session-name} \033[0m[-w {num_windows} | -e | -k | -s | -h]"
+	echo -e "\033[36m\nMandatory argument:\033[0m"
+	echo -e " \033[33m-n\033[0m | Tmux Session Name"
+	echo -e "\033[36m\nOptional arguments:\033[0m"
+	echo -e " \033[35m-w\033[0m | Specify number of horizontally-split windows to open (default 1, maximum 5)"
+	echo -e " \033[35m-k\033[0m | Use the existing .tmux.conf file instead of an optimized one"
+	echo -e " \033[35m-s\033[0m | Swift and Slient mode (disables status messages)"
+	echo -e " \033[35m-e\033[0m | Run engagementinit.sh before newtmux begins (Alias required)"
+	echo -e " \033[35m-h\033[0m | Display this help menu"
+	echo -e "\n\033[36mExample execution (with an alias in ~/.zshrc or ~/.bashrc):"
+	echo -e " \033[32m$SCRIPT_NAME \033[33m-n\033[0m flast \033[35m-w\033[0m 2 \033[35m-e\033[0m"
 	return 0
 }
 
@@ -185,8 +185,8 @@ do
 		w) # Number of horizontally-split windows to open (default 1, maximum 5)
 			if ! [[ $OPTARG -ge 1 && $OPTARG -le 5 ]]
 			then
-				echo -e "Error — \e[31mInvalid argument\e[0m for option \e[35m-w"
-				echo -e "\e[36mMin:\e[0m 1, \e[36mMax:\e[0m 5, \e[36mDefault (no flag):\e[0m 1"
+				echo -e "Error — \033[31mInvalid argument\033[0m for option \033[35m-w"
+				echo -e "\033[36mMin:\033[0m 1, \033[36mMax:\033[0m 5, \033[36mDefault (no flag):\033[0m 1"
 				exit 1
 			fi
 			NUM_WINDOWS=$OPTARG;;
@@ -200,17 +200,17 @@ do
 			help
 			exit 0;;
 		\?)# Invalid option
-			echo -e "Error — \e[31mUnknown option provided.\e[0m Exiting..."
+			echo -e "Error — \033[31mUnknown option provided.\033[0m Exiting..."
 			exit 1;;
 		:) # Missing argument
-			echo -e "Error — \e[31mMissing option argument\e[0m for\e[35m -$OPTARG\e[0m Exiting..."
+			echo -e "Error — \033[31mMissing option argument\033[0m for\033[35m -$OPTARG\033[0m Exiting..."
 			exit 1;;
 	esac
 done
 
 if [[ $SESSION_NAME =~ ^-.$ || -z $SESSION_NAME ]]
 then
-	echo -e "Error — \e[31mYou must provide a session name \e[0mfor \e[32m$SCRIPT_NAME\e[0m to create.\n"
+	echo -e "Error — \033[31mYou must provide a session name \033[0mfor \033[32m$SCRIPT_NAME\033[0m to create.\n"
 	help
 	exit 1
 fi
